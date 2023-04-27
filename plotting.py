89cd03760b9ef6dd118879249c8e98ee645a7355
Ana Trisovic <ana.trisovic@gmail.com>
2023-04-27 12:19:11 -0400

    add zip code hotspots

--- plotting.py
+++ plotting.py
@@ -7,6 +7,9 @@ sns.set_context("paper")
 import plotly.express as px
 import matplotlib.pyplot as plt
 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import contextily as ctx 
+
 from urllib.request import urlopen
 import json
 
@@ -18,24 +21,63 @@ def get_counties():
 
 def get_hotspopt_dict():
     hsdict = {
-        "hwp":"Total days of Heat & Wildfire & Pollution",
-        "hws":"Total days of Heat & Wildfire & Smoke PM 25",
-        "hp":"Total days of Heat & Pollution",
-        "hw":"Total days of Heat & Wildfire",
-        "ws":"Total days of Wildfire & Smoke PM 25",
-                  }
+        'wfday':{"title":"wildfire", "title_map":"Total days of wildfire", "year_min":'2007', "year_max":'2022'},
+        'heatday':{"title":"heat", "title_map":"Total days of heat", "year_min":'2007', "year_max":'2022'},
+        'coldday':{"title":"cold", "title_map":"Total cold days", "year_min":'2007', "year_max":'2022'},
+        'polluted':{"title":"pollution", "title_map":"Total days of pollution", "year_min":'2007', "year_max":'2021'},
+        'smoke_polluted':{"title":"smoke pollution", "title_map":"Total days of smoke pollution", "year_min":'2007', "year_max":'2021'},
+        "hwp":{"title":"heat & wildfire & pollution", "title_map":"Total days of heat & wildfire & pollution", "year_min":'2007', "year_max":'2021'},
+        "hws":{
+            "title":"heat & wildfire & smoke pollution",
+            "title_map":"Total days of heat & wildfire & smoke pollution",
+            "year_min":'2007',
+            "year_max":'2021'},
+        "hp":{"title":"heat & pollution", "title_map":"Total days of heat & pollution", "year_min":'2007', "year_max":'2021'},
+        "hw":{"title":"heat & wildfire", "title_map":"Total days of heat & wildfire", "year_min":'2007', "year_max":'2022'},
+        "ws":{
+            "title":"wildfire & smoke pollution", 
+            "title_map":"Total days of wildfire & smoke pollution", "year_min":'2007', "year_max":'2021'},      
+        'hwps':{
+            "title":"heat or wildfire or pollution",  
+            "title_map":"Totay days of heat or wildfire or pollution",
+            "year_min":'2007',
+            "year_max":'2021'}
+    }
     return hsdict
 
+
+def statePlot(df, data, title, cmap, zoom, dpi):
+    f,ax = plt.subplots(1,1, figsize=(8,8), 
+    sharex=True,sharey=True, dpi=dpi)
+    f.tight_layout(pad=0.8)
+    ax.set_axis_off()
+    plt.title(title,fontsize='large')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="3%", pad=0.2)
+    ux = df.to_crs(epsg=3857).plot(data, ax=ax, 
+    edgecolor='k',    cmap=cmap, alpha = 1, linewidth=0.1,
+    legend=True, cax=cax)
+    #ctx.add_basemap(ux,    zoom=zoom,
+    # source=ctx.providers.Stamen.TonerLite);
+    plt.ylabel('Days of exposure', fontsize=12)
+    # Use savefig to save your map
+    plt.tight_layout()
+    plt.savefig(
+    'figures/zip_hotspots_' + data +'.png')
+    plt.show()
+
+    
 def trend_plot(trends_df, l, hotspot, title, year_min='2006', year_max='2021'):
-    plt.figure(1, figsize = (4,3))
-    sns.lineplot(data = trends_df, x="time", y=hotspot)
+    f, ax = plt.subplots(1,1, figsize = (4,3))
+    sns.lineplot(data = trends_df, x="time", y=hotspot, ax=ax)
 
     sns.lineplot(data = trends_df[
         trends_df.COUNTY_CODE.isin(l)], 
                  x="time", 
                  y=hotspot, 
                  hue="COUNTY_CODE", 
-                 palette="tab10")
+                 palette="tab10",
+                 ax=ax)
 
     plt.xlabel("Year")
     plt.ylabel("Number of days of concurrence")
